29088dc86a0ef6a2896538900016f7d1917a6381
Macodou DIOUF <mac.koonda@gmail.com>
2018-12-08 15:23:35 +0000

    Création du projet et ajout de quelques dependances

---
+++ src/main/java/ma/agile/ocp/AgileOcpApplication.java
@@ -0,0 +1,12 @@
+package ma.agile.ocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileOcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileOcpApplication.class, args);
+	}
+}
