a32aea8e62cafa7ca0196f1a6e85987c92e5f8f3
Macodou DIOUF <mac.koonda@gmail.com>
2018-12-08 20:10:04 +0000

    Implementation gestion des erreurs du flux WS create project

--- src/main/java/ma/agile/ocp/controllers/ProjetController.java
+++ src/main/java/ma/agile/ocp/controllers/ProjetController.java
@@ -1,8 +1,16 @@
 package ma.agile.ocp.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +27,17 @@ public class ProjetController {
 	private ProjetService projetService;
 	
 	@PostMapping("")
-	public ResponseEntity<Projet> createNewProjet(@RequestBody Projet projet){
+	public ResponseEntity<?> createNewProjet(@Valid @RequestBody Projet projet, BindingResult result){
+		if(result.hasErrors()){
+			
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for(FieldError error: result.getFieldErrors()){
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+		}
 		Projet newProjet = projetService.saveOrUpdate(projet);
-		return new ResponseEntity<Projet>(projet, HttpStatus.CREATED);
+		return new ResponseEntity<Projet>(newProjet, HttpStatus.CREATED);
 	}
 }
